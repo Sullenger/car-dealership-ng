c22b1919eef85096efdf287f45791cbfe50c1856
Accidental82 <Jason.sullenger@gmail.com>
2016-04-06 12:14:39 -0600

    minor refactoring

--- assets/apps/components/main-controller.js
+++ assets/apps/components/main-controller.js
@@ -2,33 +2,8 @@ app.controller('MainController', function($scope, $http) {
     $scope.carLot = [];
 
     $http.get('assets/packets/vehicles.json').success(function(data) {
-        
         data.forEach(function(key) {
-            // debugger;
             this.push(key);
         }, $scope.carLot);
-
     });
-
 });
-
-// beautyApp.controller('ubeautycntrl',function($scope,$http){
-
-//     $http.get('http://localhost/Find-Beauty/media/services.json').success(function(data) {
-//         $scope.services=data.services;
-//         $scope.services1=data.services1;
-//     });
-
-//     $scope.Add = function(){
-
-//         $http.get('http://localhost/Find-Beauty/media/services_show.json').success(function(data) {
-//             console.log(angular.toJson(data.services));
-//             $scope.services.push(data.services);
-
-//         });
-
-//     };
-
-//     $scope.ViewMore = function(){
-
-// });
