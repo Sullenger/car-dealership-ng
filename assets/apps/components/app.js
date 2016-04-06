8a80ac75f035f4a5b2ebfe952b6a878f97ba767c
Accidental82 <Jason.sullenger@gmail.com>
2016-04-06 12:04:05 -0600

    adds sort and filter

--- assets/apps/components/app.js
+++ assets/apps/components/app.js
@@ -1,41 +1,42 @@
+var app = angular.module('carDealership', [])
 
-var requestor = function() {
-    var url = "assets/packets/vehicles.json";
-    $.get(url).success(function(res) {
-        drawCollection(res)
-    })
-}
+// var requestor = function() {
+//     var url = "assets/packets/vehicles.json";
+//     $.get(url).success(function(res) {
+//         pushCollection(res)
+//     })
+// }
 
-function drawCollection(collection) {
+// function pushCollection(collection) {
 
-    for (var i = 0; i < collection.length; i++) {
-        $('.cars').append('<div class="panel car panel-success"><div class="panel-heading"><h3 class="panel-title cars-title' + [i] + '"></h3></div><div class="panel-body cars-body' + [i] + '"></div></ div>');
+    // for (var i = 0; i < collection.length; i++) {
+    //     $('.cars').append('<div class="panel car panel-success"><div class="panel-heading"><h3 class="panel-title cars-title' + [i] + '"></h3></div><div class="panel-body cars-body' + [i] + '"></div></ div>');
 
-        $('.cars-title' + [i] + '').append
-            ('<h4>' + collection[i].year + ' ' + collection[i].make + ' - ' + collection[i].model + ' ' + collection[i].price + '</h4>');
-        $('.cars-body' + [i] + '').append
-            ('<p>' + 'Description: ' + collection[i].description + '<br/>' + 'Color: ' + collection[i].color + '<br/>' + 'Odometer: ' + collection[i].miles + '<br/>' + 'Vehicle VIN: ' + collection[i]._id + '<br/>' + 'Fuel Economy: ' + collection[i].highwayMileage.high + '-' + collection[i].highwayMileage.low + 'mpg' + '</p>');
-    }
-}
+    //     $('.cars-title' + [i] + '').append
+    //         ('<h4>' + collection[i].year + ' ' + collection[i].make + ' - ' + collection[i].model + ' ' + collection[i].price + '</h4>');
+    //     $('.cars-body' + [i] + '').append
+    //         ('<p>' + 'Description: ' + collection[i].description + '<br/>' + 'Color: ' + collection[i].color + '<br/>' + 'Odometer: ' + collection[i].miles + '<br/>' + 'Vehicle VIN: ' + collection[i]._id + '<br/>' + 'Fuel Economy: ' + collection[i].highwayMileage.high + '-' + collection[i].highwayMileage.low + 'mpg' + '</p>');
+    // }
+// }
 
-requestor();
+// requestor();
 
 
-$(document).ready(function() {
-    // $('#btnSort').click(function(e) {
-    //     $("#ddlList").html($('#ddlList option').sort(function(x, y) {
-    //         return $(x).text() < $(y).text() ? -1 : 1;
-    //     }))
-    //     $("#ddlList").get(0).selectedIndex = 0;
-    //     e.preventDefault();
-    // });
+// $(document).ready(function() {
+//     // $('#btnSort').click(function(e) {
+//     //     $("#ddlList").html($('#ddlList option').sort(function(x, y) {
+//     //         return $(x).text() < $(y).text() ? -1 : 1;
+//     //     }))
+//     //     $("#ddlList").get(0).selectedIndex = 0;
+//     //     e.preventDefault();
+//     // });
 
-    $('#priceSort').click(function(e) {
-        $('.cars-title').html($('collection[i].year').sort(function(x, y) {
-            debugger;
-            return $(x).val() < $(y).val() ? -1 : 1;
-        }))
-        $("#lotList").get(0).selectedIndex = 0;
-        e.preventDefault();
-    });
-});
+//     $('#priceSort').click(function(e) {
+//         $('.cars-title').html($('collection[i].year').sort(function(x, y) {
+//             debugger;
+//             return $(x).val() < $(y).val() ? -1 : 1;
+//         }))
+//         $("#lotList").get(0).selectedIndex = 0;
+//         e.preventDefault();
+//     });
+// });
