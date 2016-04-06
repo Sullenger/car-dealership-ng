70ebe2ed0593ad76598460615d14bac600bebe54
Accidental82 <Jason.sullenger@gmail.com>
2016-04-05 21:26:02 -0600

    broken

--- assets/apps/components/app.js
+++ assets/apps/components/app.js
@@ -9,22 +9,33 @@ var requestor = function() {
 function drawCollection(collection) {
 
     for (var i = 0; i < collection.length; i++) {
-        $('.cars').append('<div class="panel car panel-success"><div class="panel-heading"><h3 class="panel-title cars-title'+ [i] +'"></h3></div><div class="panel-body cars-body'+ [i] +'"></div></ div>' );
-        
-        $('.cars-title'+ [i] +'').append
-            ('<h4>' + collection[i].year + ' ' + collection[i].make + ' - ' + collection[i].model  + ' ' + collection[i].price+ '</h4>');
-        $('.cars-body'+ [i] +'').append
-            ('<p>' +'Description: ' + collection[i].description +'<br/>'+'Color: ' + collection[i].color + '<br/>'+'Odometer: ' + collection[i].miles +'<br/>'+'Vehicle VIN: ' + collection[i]._id + '<br/>'+'Fuel Economy: ' + collection[i].highwayMileage.high + '-'+ collection[i].highwayMileage.low +'mpg' + '</p>');
+        $('.cars').append('<div class="panel car panel-success"><div class="panel-heading"><h3 class="panel-title cars-title' + [i] + '"></h3></div><div class="panel-body cars-body' + [i] + '"></div></ div>');
+
+        $('.cars-title' + [i] + '').append
+            ('<h4>' + collection[i].year + ' ' + collection[i].make + ' - ' + collection[i].model + ' ' + collection[i].price + '</h4>');
+        $('.cars-body' + [i] + '').append
+            ('<p>' + 'Description: ' + collection[i].description + '<br/>' + 'Color: ' + collection[i].color + '<br/>' + 'Odometer: ' + collection[i].miles + '<br/>' + 'Vehicle VIN: ' + collection[i]._id + '<br/>' + 'Fuel Economy: ' + collection[i].highwayMileage.high + '-' + collection[i].highwayMileage.low + 'mpg' + '</p>');
     }
 }
 
 requestor();
 
-// var inHTML = "";
 
-// $.each(people.persons, function(index, value){
-//     var newItem = "<tr><td>"+ value.name + "</td></tr>"
-//     inHTML += newItem;  
-// });
+$(document).ready(function() {
+    // $('#btnSort').click(function(e) {
+    //     $("#ddlList").html($('#ddlList option').sort(function(x, y) {
+    //         return $(x).text() < $(y).text() ? -1 : 1;
+    //     }))
+    //     $("#ddlList").get(0).selectedIndex = 0;
+    //     e.preventDefault();
+    // });
 
-// $("table#dynamicTable").html(inHTML);
+    $('#priceSort').click(function(e) {
+        $('.cars-title').html($('collection[i].year').sort(function(x, y) {
+            debugger;
+            return $(x).val() < $(y).val() ? -1 : 1;
+        }))
+        $("#lotList").get(0).selectedIndex = 0;
+        e.preventDefault();
+    });
+});
