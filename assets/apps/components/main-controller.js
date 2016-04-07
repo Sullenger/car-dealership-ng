6ffd950991ef31d8f64bc66c37a5f7e85a9b5f1e
Accidental82 <Jason.sullenger@gmail.com>
2016-04-06 19:56:19 -0600

    adds styling and fab

--- assets/apps/components/main-controller.js
+++ assets/apps/components/main-controller.js
@@ -3,6 +3,8 @@ app.controller('MainController', function($scope, $http) {
 
     $http.get('assets/packets/vehicles.json').success(function(data) {
         data.forEach(function(key) {
+            var currency = key.price;
+            key.price = Number(currency.replace(/[^0-9\.]+/g,""));
             this.push(key);
         }, $scope.carLot);
     });
