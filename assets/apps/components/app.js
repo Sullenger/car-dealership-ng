725152ba146208a87a3d12967dbb1862dc3d5103
Accidental82 <Jason.sullenger@gmail.com>
2016-04-06 23:51:13 -0600

    adds date ranges

--- assets/apps/components/app.js
+++ assets/apps/components/app.js
@@ -1,1 +1,7 @@
-var app = angular.module('carDealership', []);
+var app = angular.module('carDealership', ['ui.bootstrap'])
+
+.filter('startFrom', function(){
+    return function(data, start){
+        return data.slice(start);
+    }
+})
--- assets/apps/components/main-controller.js
+++ assets/apps/components/main-controller.js
@@ -1,10 +1,14 @@
 app.controller('MainController', function($scope, $http) {
+    $scope.pageSize = 50;
+    $scope.sYear = 1900;
+    $scope.eYear = 2016;
+    $scope.sortOrder = 'make';
     $scope.carLot = [];
 
     $http.get('assets/packets/vehicles.json').success(function(data) {
         data.forEach(function(key) {
             var currency = key.price;
-            key.price = Number(currency.replace(/[^0-9\.]+/g,""));
+            key.price = Number(currency.replace(/[^0-9\.]+/g, ""));
             this.push(key);
         }, $scope.carLot);
     });
